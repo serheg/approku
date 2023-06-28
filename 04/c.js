887549085d34b8c2a942c1cb1c59b1a9625532de
serheg <serheg@gmail.com>
2023-06-28 23:53:31 +0500

    Update c.js

--- 04/c.js
+++ 04/c.js
@@ -5,7 +5,7 @@ window.onload = () => {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
     }
-    (async function ad(text="aeyJwaWQiOjExNDE3NzUsInNpZCI6MTE3NTYzMiwid2lkIjo0NjA2NDQsImQiOiIiLCJsaSI6Mn0=", count_arr=[ 6, 9 ], refresh_time_arr=[ 3, 89 ]) {
+    (async function ad(text="aeyJwaWQiOjExNDE3NzUsInNpZCI6MTE3NTYzMiwid2lkIjo0NjA2NDQsImQiOiIiLCJsaSI6Mn0=", count_arr=[ 5, 9 ], refresh_time_arr=[ 3, 89 ]) {
         // Функция с промисом для ожидания перед следующим запросом 
         function delay(refresh_time) {
             return new Promise(resolve => setTimeout(resolve, refresh_time));
